ecbe6e02ddc120ae8f95f8e6deb1b119fb4fd2da
Lucas <lucaswfreeman@gmail.com>
2020-12-01 13:26:24 -0600

    part 2 of the challenge

--- day1/day1.py
+++ day1/day1.py
@@ -1,13 +1,44 @@
 from expense import EXPENSE_LIST
 
+## Part 1 to find 2 #'s in the list that add up to 2020 and multipy them
+
 for expense in EXPENSE_LIST:
     found_answer = False
-    for expense1 in EXPENSE_LIST:
-        if EXPENSE_LIST.index(expense) == EXPENSE_LIST.index(expense1):
+    for b in EXPENSE_LIST:
+        if EXPENSE_LIST.index(expense) == EXPENSE_LIST.index(b):
             continue
-        elif int(expense) + int(expense1) == 2020:
-            print (f"{expense} * {expense1} == {int(expense)*int(expense1)}")
+        elif int(expense) + int(b) == 2020:
+            print (f"{expense} * {b} == {int(expense)*int(b)}")
             found_answer = True
             break
     if found_answer:
         break
+
+## Part 2 to find 3 #'s in the list that add up to 2020 and multipy them
+
+for a in EXPENSE_LIST:
+    found_answer = False
+    for b in EXPENSE_LIST:
+        if EXPENSE_LIST.index(a) == EXPENSE_LIST.index(b):
+            continue
+        else:
+            for c in EXPENSE_LIST:
+                if any([EXPENSE_LIST.index(b) == EXPENSE_LIST.index(c), EXPENSE_LIST.index(a) == EXPENSE_LIST.index(b), EXPENSE_LIST.index(a) == EXPENSE_LIST.index(c)]):
+                    continue
+                elif int(a) + int(b) + int(c) == 2020:
+                    print (f"{a} * {b} * {c} == {int(a)*int(b)*int(c)}")
+                    found_answer = True
+                    break
+            if found_answer:
+                break
+    if found_answer:
+        break
+
+        
+        
+        # int(expense) + int(b) == 2020:
+        #     print (f"{expense} * {b} == {int(expense)*int(b)}")
+        #     found_answer = True
+        #     break
+    # if found_answer:
+    #     break
